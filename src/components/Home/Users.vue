e5b902cc2682b8773d2b395a61f93d2de186387e
Onejohi <tonniewanjohi@gmail.com>
2021-06-29 02:25:58 +0300

    - users basic page

--- src/components/Home/Users.vue
+++ src/components/Home/Users.vue
@@ -1,5 +1,22 @@
 <template>
-  <h2>Users</h2>
+  <div class="container-fluid">
+    <div class="row row-cols-1 row-cols-md-3">
+      <div class="col col-12 col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-3">
+                <img class="rounded-circle" src="https://ui-avatars.com/api/?name=John+Doe?size=40" alt="">
+              </div>
+              <div class="col-9">
+                <h4 class="mt-3">John Doe</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
