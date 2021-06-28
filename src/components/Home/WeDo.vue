a972abbf5c56fca5f8563f4ee158cf06a4404e77
Onejohi <tonniewanjohi@gmail.com>
2021-06-29 02:19:56 +0300

    - install bootstrap icons - set up wedo initial UI

--- src/components/Home/WeDo.vue
+++ src/components/Home/WeDo.vue
@@ -1,5 +1,26 @@
 <template>
-  <h2>We Do</h2>
+  <div class="container-fluid">
+      <div class="row mt-2 row-cols-1 row-cols-md-3">
+        <div class="col col-12 col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Task title</h5>
+              <p class="card-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis nesciunt repellendus dolores modi optio eaque animi dolore mollitia! Nesciunt, a?
+              </p>
+              12
+              <button class="btn mx-2 btn-light">
+                <i class="bi bi-heart-fill text-danger"></i>
+              </button>
+              5
+              <button class="btn mx-2 btn-light">
+                <i class="bi bi-arrow-down-square text-secondary"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
